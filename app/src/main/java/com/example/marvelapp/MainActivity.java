ae8e213f9e578ec582ce65aac9959f7c3a1e4910
Brandon Corpas <corpasdev@gmail.com>
2024-06-07 10:14:13 -0500

    login y registro configurado

--- app/src/main/java/com/example/marvelapp/MainActivity.java
+++ app/src/main/java/com/example/marvelapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.marvelapp;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
+
+    FirebaseAuth auth;
+    Button button;
+    TextView textView;
+    FirebaseUser user;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +35,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+//        auth = FirebaseAuth.getInstance();
+//        button = findViewById(R.id.logout);
+//        textView = findViewById(R.id.user_detalle);
+//        user = auth.getCurrentUser();
+//        if(user == null){
+//            Intent intent = new Intent(getApplicationContext(), Login.class);
+//            startActivity(intent);
+//            finish();
+//        }
+//
+//        else {
+//            textView.setText(user.getEmail());
+//        }
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FirebaseAuth.getInstance().signOut();
+//                Intent intent = new Intent(getApplicationContext(), Login.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
+
+
+
+
+
+
     }
 }
